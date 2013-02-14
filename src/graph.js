5f925890f85debfa56d8622ace13da095d507815
Shiran Pasternak <shiranpasternak@gmail.com>
2013-02-14 15:40:42 -0500

    Allow graph JSON input.

--- src/graph.js
+++ src/graph.js
@@ -3,8 +3,8 @@ if (typeof define !== 'function') {
 }
 
 define(function (require) {
-    function Node(graph, id) {
-    	var meta = {};
+    var Node = function (graph, id, meta) {
+    	var _meta = {};
     	var node = {
             id: id,
     		neighbors: function () {
@@ -15,11 +15,20 @@ define(function (require) {
                 return this;
     		},
     		attribute: function (key, val) {
-    			meta[key] = val;
+    			_meta[key] = val;
                 return this;
     		},
-    		meta: function () { return meta; }
+    		meta: function (d) {
+                if (d) {
+                    for (var prop in d) {
+                        _meta[prop] = d[prop];
+                    }
+                    return this;
+                }
+                return _meta;
+            }
     	};
+        if (meta) node.meta(meta);
     	return node;
     }
 
@@ -27,26 +36,34 @@ define(function (require) {
         return !isNaN(parseInt(val)) && (parseFloat(val) == parseInt(val)); 
     }
 
-    function Graph(data) {
+    var _INDEXED = new Object(1);
+    var Graph = function (data, type) {
+        var self = this;
         var idSequence = 0;
         var nodes = {};
         var edges = {};
         var nodelist = [];
-        
-        this.link = function (n1, n2, meta) {
+        var nodeCount = 0;
+                
+        self.link = function (n1, n2, meta) {
             n1 = isInt(n1) ? n1 : n1.id;
             n2 = isInt(n2) ? n2 : n2.id;
             var key = [n1, n2].sort().join(" ");
             edges[key] = { source: nodes[n1], target: nodes[n2], meta: meta }
             return this;
         }
-        this.addNode = function (id) {
-            id = (id || idSequence++);
+        self.addNode = function (id) {
+            id = id || idSequence++;
             var node = new Node(this, id);
             nodes[node.id] = node;
             return node;
         }
-        this.neighbors = function (node) {
+        self.addEdge = function (edge) {
+            return type == _INDEXED
+                ? self.link(nodes[edge.source], nodes[edge.target], edge)
+                : self.link(edge.source, edge.target, edge);
+        }
+        self.neighbors = function (node) {
             var arr = [];
             for (var key in edges) {
                 var edge = edges[key];
@@ -55,17 +72,17 @@ define(function (require) {
             }
             return arr;
         }
-        this.nodes = function () {
+        self.nodes = function () {
             var arr = [];
             for (var k in nodes) { arr.push(nodes[k]); }
             return arr;
         }
-        this.edges = function () {
+        self.edges = function () {
             var arr = [];
             for (var k in edges) { arr.push(edges[k]); }
             return arr;
         }
-        this.json = function () {
+        self.json = function () {
             var jsonNodes = [];
             var jsonEdges = [];
             var nodeIndex = {};
@@ -73,7 +90,7 @@ define(function (require) {
             for (var k in nodes) {
                 var node = nodes[k];
                 var meta = node.meta();
-                meta.id = node.id;
+                meta.id = meta.id || node.id;
                 nodeIndex[node.id] = jsonNodes.length;
                 jsonNodes.push(meta);
             }
@@ -91,13 +108,27 @@ define(function (require) {
             }
             return { nodes: jsonNodes, edges: jsonEdges };
         }
-        if (data) {
-            if (data.nodes) { this.nodes(data.nodes); }
-            if (data.edges) { this.edges(data.edges); }
+
+        function initializeData(graph) {
+            if (graph.nodes) {
+                graph.nodes.forEach(function (meta) {
+                    var node = new Node(self, idSequence++, meta);
+                    if (type == _INDEXED)
+                        nodes[nodeCount++] = node;
+                    else
+                        nodes[node.id] = node;
+                })
+            }
+            if (graph.edges) {
+                graph.edges.forEach(function (edge) {
+                    self.addEdge(edge);
+                })
+            }
         }
-        
-        return this;
+        if (data) initializeData(data);
+        return self;
     }
+    Graph.INDEXED = _INDEXED;
 
     return Graph;
 })
